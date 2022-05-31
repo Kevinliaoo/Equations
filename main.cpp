70b9db9d61b0c73d43db8420bf52cf53c9d59869
Kevin Liao <liaok0082@gmail.com>
2022-05-31 15:26:20 +0800

    main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,23 @@ using namespace std;
 
 int main()
 {
+    // Polynomial functions
+    vector<double> coefs;
+    coefs.push_back(1);
+    coefs.push_back(2);
+    coefs.push_back(3);
+    // 3x^2 + 2x + 1
+    Polynomial pol(coefs);
+    cout << pol << endl;
+
+    // Run f(2)
+    cout << pol.f(2) << endl;
+
+    // Trigonometric functions
+    // y = 1 * sin(x)
     Sine sin;
-    Cosine cos;
+    // y = 2 * cos(10x - 0.5) + 5
+    Cosine cos(2, 10, 0.5, 5);
     Tangent tan;
 
     cout << sin.f(0) << endl;
